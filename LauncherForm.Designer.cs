13e2cf63fe8a54bd9023c6abf57d557643ef281b
ld8o <editorx990@gmail.com>
2025-08-09 01:03:29 +0300

    Initial commit

---
+++ LauncherForm.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace LDMC
+{
+    partial class LauncherForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LauncherForm));
+            label1 = new Label();
+            cbVersion = new ComboBox();
+            pbFiles = new ProgressBar();
+            pbProgress = new ProgressBar();
+            lbProgress = new Label();
+            btnStart = new Button();
+            usernameInput = new TextBox();
+            label2 = new Label();
+            minecraftVersion = new ComboBox();
+            characterPictureBox = new PictureBox();
+            label3 = new Label();
+            characterHelpPictureBox = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)characterPictureBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)characterHelpPictureBox).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 53);
+            label1.Name = "label1";
+            label1.Size = new Size(82, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Select version:";
+            // 
+            // cbVersion
+            // 
+            cbVersion.FormattingEnabled = true;
+            cbVersion.Location = new Point(12, 71);
+            cbVersion.Name = "cbVersion";
+            cbVersion.Size = new Size(147, 23);
+            cbVersion.TabIndex = 1;
+            cbVersion.TextChanged += cbVersion_TextChanged;
+            // 
+            // pbFiles
+            // 
+            pbFiles.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            pbFiles.Location = new Point(12, 148);
+            pbFiles.Name = "pbFiles";
+            pbFiles.Size = new Size(380, 20);
+            pbFiles.TabIndex = 3;
+            // 
+            // pbProgress
+            // 
+            pbProgress.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            pbProgress.Location = new Point(12, 177);
+            pbProgress.Name = "pbProgress";
+            pbProgress.Size = new Size(380, 20);
+            pbProgress.TabIndex = 4;
+            // 
+            // lbProgress
+            // 
+            lbProgress.Location = new Point(12, 130);
+            lbProgress.Name = "lbProgress";
+            lbProgress.Size = new Size(293, 15);
+            lbProgress.TabIndex = 5;
+            // 
+            // btnStart
+            // 
+            btnStart.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            btnStart.Location = new Point(12, 100);
+            btnStart.Margin = new Padding(0);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(293, 25);
+            btnStart.TabIndex = 7;
+            btnStart.Text = "Launch";
+            btnStart.UseVisualStyleBackColor = true;
+            btnStart.Click += btnStart_Click;
+            // 
+            // usernameInput
+            // 
+            usernameInput.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            usernameInput.Location = new Point(12, 27);
+            usernameInput.Name = "usernameInput";
+            usernameInput.Size = new Size(293, 23);
+            usernameInput.TabIndex = 9;
+            usernameInput.TextChanged += usernameInput_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(63, 15);
+            label2.TabIndex = 8;
+            label2.Text = "Username:";
+            // 
+            // minecraftVersion
+            // 
+            minecraftVersion.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            minecraftVersion.FormattingEnabled = true;
+            minecraftVersion.Items.AddRange(new object[] { "Releases and Installed", "All Versions" });
+            minecraftVersion.Location = new Point(165, 71);
+            minecraftVersion.Name = "minecraftVersion";
+            minecraftVersion.Size = new Size(140, 23);
+            minecraftVersion.TabIndex = 10;
+            minecraftVersion.Text = "Releases and Installed";
+            minecraftVersion.SelectedIndexChanged += minecraftVersion_SelectedIndexChanged;
+            // 
+            // characterPictureBox
+            // 
+            characterPictureBox.BackColor = Color.Transparent;
+            characterPictureBox.Image = Properties.Resources.Steve_classic;
+            characterPictureBox.Location = new Point(311, 27);
+            characterPictureBox.Name = "characterPictureBox";
+            characterPictureBox.Size = new Size(81, 115);
+            characterPictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            characterPictureBox.TabIndex = 11;
+            characterPictureBox.TabStop = false;
+            characterPictureBox.Tag = "Steve classic";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(311, 9);
+            label3.Name = "label3";
+            label3.Size = new Size(76, 15);
+            label3.TabIndex = 12;
+            label3.Text = "Skin preview:";
+            // 
+            // characterHelpPictureBox
+            // 
+            characterHelpPictureBox.Image = Properties.Resources.icon_info;
+            characterHelpPictureBox.Location = new Point(376, 27);
+            characterHelpPictureBox.Name = "characterHelpPictureBox";
+            characterHelpPictureBox.Size = new Size(16, 16);
+            characterHelpPictureBox.TabIndex = 13;
+            characterHelpPictureBox.TabStop = false;
+            characterHelpPictureBox.Tag = "The game will choose your offline skin from your username";
+            // 
+            // LauncherForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(404, 211);
+            Controls.Add(characterHelpPictureBox);
+            Controls.Add(label3);
+            Controls.Add(characterPictureBox);
+            Controls.Add(minecraftVersion);
+            Controls.Add(usernameInput);
+            Controls.Add(label2);
+            Controls.Add(btnStart);
+            Controls.Add(lbProgress);
+            Controls.Add(pbProgress);
+            Controls.Add(pbFiles);
+            Controls.Add(cbVersion);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximumSize = new Size(620, 250);
+            MinimumSize = new Size(420, 250);
+            Name = "LauncherForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "LDMC";
+            Load += LauncherForm_Load;
+            ((System.ComponentModel.ISupportInitialize)characterPictureBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)characterHelpPictureBox).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ComboBox cbVersion;
+        private ProgressBar pbFiles;
+        private ProgressBar pbProgress;
+        private Label lbProgress;
+        private Button btnStart;
+        private ComboBox minecraftVersion;
+        private TextBox usernameInput;
+        private Label label2;
+        private PictureBox characterPictureBox;
+        private Label label3;
+        private PictureBox characterHelpPictureBox;
+    }
+}
---
+++ LauncherForm.cs
@@ -0,0 +1,211 @@
+using CmlLib.Core;
+using CmlLib.Core.Auth;
+using CmlLib.Core.Installers;
+using CmlLib.Core.ProcessBuilder;
+using CmlLib.Core.VersionMetadata;
+
+namespace LDMC;
+
+public partial class LauncherForm : Form
+{
+    private readonly MinecraftLauncher _launcher;
+    private string _playerUuid = string.Empty;
+    private MinecraftPath _launcherPath;
+    private string _launcherProfilesPath => _launcherPath.BasePath + "/launcher_profiles.json";
+
+    private readonly ToolTip _characterPreviewTooltip = new ToolTip();
+    private readonly ToolTip _helpTooltip = new ToolTip();
+
+    public LauncherForm()
+    {
+        // Make MinecraftLauncher with default minecraft path
+        _launcherPath = new MinecraftPath();
+        _launcherPath.CreateDirs();
+        _launcher = new MinecraftLauncher(_launcherPath);
+
+        // Create launcher_profiles.json if it does not exist in the launcher path
+        if (!File.Exists(_launcherProfilesPath))
+            File.WriteAllText(_launcherProfilesPath, "{\"profiles\":{}}");
+
+        // Attach events with functions
+        _launcher.FileProgressChanged += _launcher_FileProgressChanged;
+        _launcher.ByteProgressChanged += _launcher_ByteProgressChanged;
+
+        // Load UI
+        InitializeComponent();
+    }
+
+    private async void LauncherForm_Load(object sender, EventArgs e)
+    {
+        // Load previous used values in the inputs
+        usernameInput.Text = Properties.Settings.Default.Username;
+        cbVersion.Text = Properties.Settings.Default.Version;
+
+        // Set default username to Environment.UserName if empty
+        if (string.IsNullOrEmpty(usernameInput.Text))
+            usernameInput.Text = Environment.UserName;
+
+        // Adding tooltip to the character picture box and help icon
+        _helpTooltip.SetToolTip(characterHelpPictureBox, characterHelpPictureBox.Tag?.ToString());
+        _characterPreviewTooltip.SetToolTip(characterPictureBox, characterPictureBox.Tag?.ToString());
+
+        // When loaded, list all versions
+        await listVersions();
+    }
+
+    private async Task listVersions(bool includeAll = false)
+    {
+        // Clear list
+        cbVersion.Items.Clear();
+
+        // List all versions
+        var versions = await _launcher.GetAllVersionsAsync();
+        foreach (var version in versions)
+        {
+            // Check if includeAll is enabled
+            if (version.GetVersionType() == MVersionType.Release ||
+                version.GetVersionType() == MVersionType.Custom ||
+                includeAll)
+            {
+                cbVersion.Items.Add(version.Name);
+            }
+        }
+
+        // Default latest if not already set
+        if (string.IsNullOrEmpty(cbVersion.Text))
+            cbVersion.Text = versions.LatestReleaseName;
+    }
+
+    private async void btnStart_Click(object sender, EventArgs e)
+    {
+        // Disable UI while launching
+        if (string.IsNullOrEmpty(cbVersion.Text))
+        {
+            MessageBox.Show("Please select a Minecraft version.");
+            return;
+        }
+
+        if (string.IsNullOrEmpty(usernameInput.Text))
+        {
+            MessageBox.Show("Please enter a username.");
+            return;
+        }
+
+        this.Enabled = false;
+        btnStart.Text = "Launching";
+
+        // Try to launch Minecraft with an Offline session
+        try
+        {
+            var session = MSession.CreateOfflineSession(usernameInput.Text);
+            session.UUID = _playerUuid;
+
+            await _launcher.InstallAsync(cbVersion.Text);
+            var process = await _launcher.BuildProcessAsync(cbVersion.Text, new MLaunchOption
+            {
+                Session = session
+            });
+            process.Start();
+
+            // Save values
+            Properties.Settings.Default.Username = usernameInput.Text;
+            Properties.Settings.Default.Version = cbVersion.Text;
+            Properties.Settings.Default.Save();
+
+            // Exit if successful
+            Application.Exit();
+        }
+        catch (Exception ex)
+        {
+            // Show error
+            MessageBox.Show($"Failed to launch Minecraft:\n{ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        finally
+        {
+            // Reset UI
+            pbProgress.Value = 0;
+            pbFiles.Value = 0;
+            lbProgress.Text = "";
+
+            this.Enabled = true;
+            btnStart.Text = "Launch";
+        }
+    }
+
+    private void _launcher_FileProgressChanged(object? sender, InstallerProgressChangedEventArgs args)
+    {
+        if (InvokeRequired)
+        {
+            Invoke(new Action(() => _launcher_FileProgressChanged(sender, args)));
+            return;
+        }
+
+        pbFiles.Maximum = args.TotalTasks;
+        pbFiles.Value = Math.Min(args.ProgressedTasks, args.TotalTasks);
+        lbProgress.Text = $"{args.Name} - {args.ProgressedTasks} / {args.TotalTasks}";
+    }
+
+    private void _launcher_ByteProgressChanged(object? sender, ByteProgress args)
+    {
+        if (InvokeRequired)
+        {
+            Invoke(new Action(() => _launcher_ByteProgressChanged(sender, args)));
+            return;
+        }
+
+        pbProgress.Maximum = 100;
+        pbProgress.Value = (int)(args.ProgressedBytes * 100 / args.TotalBytes);
+    }
+
+    private void usernameInput_TextChanged(object sender, EventArgs e)
+    {
+        // Disabling the start button if the username is empty
+        if (string.IsNullOrWhiteSpace(usernameInput.Text))
+        {
+            // If the username is empty, reset the UUID and character picture
+            _playerUuid = string.Empty;
+            _characterPreviewTooltip.SetToolTip(characterPictureBox, null);
+            characterPictureBox.Image = null;
+            characterPictureBox.Tag = null;
+            btnStart.Enabled = false; // Disable the start button if username is empty
+            return;
+        }
+        else
+        {
+            btnStart.Enabled = true;
+        }
+
+        // Update the UUID when the username changes
+        _playerUuid = Character.GenerateUuidFromUsername(usernameInput.Text);
+
+        // Update character picture box
+        updateCharacterPreview();
+    }
+
+    private async void minecraftVersion_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        // Load all versions if "All Versions" is selected
+        await listVersions(minecraftVersion.Text == "All Versions");
+    }
+
+    private void cbVersion_TextChanged(object sender, EventArgs e)
+    {
+        updateCharacterPreview();
+    }
+
+    private void updateCharacterPreview()
+    {
+        // Update the character preview based on the selected version
+        string resourceName = Character.GetCharacterResourceNameFromUuidAndGameVersion(_playerUuid, cbVersion.Text);
+        if (!string.IsNullOrEmpty(resourceName))
+        {
+            Bitmap? bitmapFromResources = (Bitmap?)Properties.Resources.ResourceManager.GetObject(resourceName);
+            if (bitmapFromResources != null)
+            {
+                characterPictureBox.Image = bitmapFromResources;
+                characterPictureBox.Tag = resourceName.Replace("_", " ");
+                _characterPreviewTooltip.SetToolTip(characterPictureBox, characterPictureBox.Tag?.ToString());
+            }
+        }
+    }
+}
